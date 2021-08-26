7a67bcc0658bc9a71dd0bf3bc0d59986fb2163ec
Riley Morris <rileymorris5@gmail.com>
2021-08-25 19:34:44 -0700

    Add project files.

---
+++ Magic_Trick.py
@@ -0,0 +1,7 @@
+import random
+print(random.sample(range(1, 100), 20))
+x = int(input("\nPick a number any number.\n\n"))
+if x > 20:
+    print("\n\nnot your day huh?")
+else:
+    print("\n\nbeginners luck...")
